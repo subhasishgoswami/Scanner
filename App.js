5d2ba6fc70a4f97e01b1f89c9b9f30e2fb0fb923
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-09-03 20:15:25 +0530

    Scanner

---
+++ App.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button, TouchableOpacity, Pressable } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Please Scan')
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })()
+  }
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data)
+    console.log('Type: ' + type + '\nData: ' + data)
+  };
+
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for Camera Permission</Text>
+      </View>)
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
+      </View>)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }} />
+      </View>
+      
+      <View style={styles.result}>
+        <Text selectable={true} style={styles.maintext}>{text}</Text>
+      </View>
+
+      {scanned && <Button title={'Scan again'} onPress={() => setScanned(false)} color='#146eb4' />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 10,
+    backgroundColor: '#146eb4',
+    elevation: 5, 
+  },
+  result:{
+    marginVertical:10,
+    width:"auto",
+    height:'auto',
+    backgroundColor: 'white',
+    elevation: 5, 
+    borderRadius: 10,
+  }
+});
